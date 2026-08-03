b3e851fa0aa56a69abb142400090ec8391693e56
Oyatulloh Gayratov <azamatgayratov771@gmaile.com>
2026-08-03 08:18:43 +0500

    toshmi kameralar iplari yengilandi

--- table-service/internal/db/migrate.go
+++ table-service/internal/db/migrate.go
@@ -146,7 +146,7 @@ func SeedBranches(db *sql.DB) error {
 			nvrIP:    "192.168.0.100",
 			nvrPort:  554,
 			nvrUser:  "admin",
-			nvrPass:  "muzaffaR7797",
+			nvrPass:  "a12345678",
 			// D9=1-stol, D8=2-stol, D12=3-stol, D14=4-stol, D5=5-stol,
 			// D11=6-stol, D10=7-stol, D10=8-stol, D3=9-stol(VIP)
 			channels: []int{9, 8, 12, 14, 5, 11, 10, 3, 2},
