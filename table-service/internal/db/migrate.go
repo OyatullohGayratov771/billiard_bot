6f6aaf0f820d48456176c9032398e61b824e840a
Oyatulloh Gayratov <azamatgayratov771@gmaile.com>
2026-09-06 07:26:58 +0500

    a

--- table-service/internal/db/migrate.go
+++ table-service/internal/db/migrate.go
@@ -147,9 +147,9 @@ func SeedBranches(db *sql.DB) error {
 			nvrPort:  554,
 			nvrUser:  "admin",
 			nvrPass:  "MuzaffaR7797",
-			// D9=1-stol, D8=2-stol, D12=3-stol, D14=4-stol, D5=5-stol,
-			// D11=6-stol, D10=7-stol, D10=8-stol, D3=9-stol(VIP)
-			channels: []int{9, 8, 12, 14, 5, 11, 10, 3, 2},
+			// D13=1-stol, D14=2-stol, D12=3-stol, D6=4-stol, D5=5-stol,
+			// D10=6-stol, D4=7-stol, D9=8-stol, D3=9-stol(VIP)
+			channels: []int{13, 14, 12, 6, 5, 10, 4, 9, 3},
 		},
 	}
 
